94341715b3c76acd45e5504a473ff245b184e4f3
Paulo Andre Araujo Vieira <paulo-andre.vieira@serpro.gov.br>
2018-05-17 12:38:43 -0300

    Adding swagger2 documentation

--- src/main/java/com/paavieira/quickstarts/App.java
+++ src/main/java/com/paavieira/quickstarts/App.java
@@ -1,7 +1,14 @@
 package com.paavieira.quickstarts;
 
+import java.util.Arrays;
+
+import com.paavieira.quickstarts.customer.model.Customer;
+import com.paavieira.quickstarts.customer.persistence.CustomerRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
@@ -9,5 +16,14 @@ public class App {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
 	}
+
+    @Bean
+    CommandLineRunner init(CustomerRepository customerRepository) {
+		return (args) -> Arrays.asList("Smith,Adam;Marx,Karl".split(";"))
+			.forEach(fullName -> {
+				final String[] names = fullName.split(",");
+				customerRepository.save(new Customer(names[1], names[0]));
+			});
+    }
 
 }
